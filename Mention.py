7697702496ef8980a9ed0b8bd58b68ed8958be73
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-08 22:35:34 +0100

    Create Mention.py

---
+++ Mention.py
@@ -0,0 +1,13 @@
+note=float(input("veuillez enter votre note pour savoir la mention :"))
+if note<10 :
+  print("insuffisant"):
+elif note>=10 and note<=12 :
+  print("passable")
+elif note>12 and note<=14 :
+  print("assez bien")
+elif note>14 and note<=16 :
+  print("bien")
+  else :
+  print("la note doit être comris entre 0 et 20")
+
+
